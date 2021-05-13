50f098ea5794b727a98d0086c0dbf787452e030e
Tim <timy0616@gmail.com>
2021-05-13 13:27:18 +0800

    update

--- UI/js/mydesign.js
+++ UI/js/mydesign.js
@@ -2,7 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var el_tabs = document.querySelector(".tabs");
     var instance_tabs = M.Tabs.init(el_tabs);
     var elems_carousel = document.querySelectorAll('.carousel');
-    var instances_carousel = M.Carousel.init(elems_carousel); 
+    var instances_carousel = M.Carousel.init(elems_carousel,{
+      dist: -80,
+      numVisible:3,
+      shift:200,
+    }); 
     /*, {
       // specify options here
     });*/
